3992ec1e96294ee3a99252bac4d2f33ed0bdd8b2
charanat <cthenuka09@gmail.com>
2025-09-08 20:59:02 +0530

    added toggle to the password filed

--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import { LiaUserShieldSolid } from "react-icons/lia";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { SlLogin } from "react-icons/sl";
 import { Link } from "react-router-dom";
+import { AiOutlineEye } from "react-icons/ai"; //eye
+import { AiOutlineEyeInvisible } from "react-icons/ai"; // eye-disab;e
 
 export default function LoginPage() {
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState(AiOutlineEyeInvisible);
+
+  const handleToggle = () => {
+    if (type === "password") {
+      setIcon(AiOutlineEye);
+      setType("text");
+    } else {
+      setIcon(AiOutlineEyeInvisible);
+      setType("password");
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="w-[430px] h-[380px] mx-auto border border-gray-400 rounded-2xl px-4 py-4 shadow-md">
@@ -25,25 +42,38 @@ export default function LoginPage() {
           </div>
           <div className="flex flex-col space-y-3">
             <div className="flex items-center">
-              <RiLockPasswordLine size={16}/>
+              <RiLockPasswordLine size={16} />
               <label className="ml-2 text-[16px] font-[600]">
                 Enter User Password:
               </label>
             </div>
-            <input
-              type="text"
-              placeholder="Enter your password"
-              className="text-[14px] border px-1.5 py-2 rounded-lg border-gray-400 focus:outline-none focus:border-blue-600"
-            />
+            <div className="relative w-full">
+              <input
+                type={type}
+                name="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full text-[14px] border px-1.5 py-2 rounded-lg border-gray-400 focus:outline-none focus:border-blue-600"
+              />
+              <span className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-600" onClick={handleToggle}>{icon}</span>
+            </div>
           </div>
 
           <div className="flex justify-between px-1">
-            <Link to={"/"} className="text-[14px] cursor-pointer">Forgot your password?</Link>
-            <Link to={"/signup"} className="underline text-[14px] cursor-pointer">Sign-Up</Link>
+            <Link to={"/"} className="text-[14px] cursor-pointer">
+              Forgot your password?
+            </Link>
+            <Link
+              to={"/signup"}
+              className="underline text-[14px] cursor-pointer"
+            >
+              Sign-Up
+            </Link>
           </div>
 
           <button className="flex items-center justify-center border p-1.5 text-white rounded-lg bg-blue-600 border-blue-600 hover:bg-blue-700 hover:border-blue-700 text-[20px] font-[600] cursor-pointer">
-            <SlLogin size={16} className="mr-2"/>
+            <SlLogin size={16} className="mr-2" />
             Login
           </button>
         </form>
